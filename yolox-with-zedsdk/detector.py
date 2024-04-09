341fb5677879dcaaa5c23476544b7953006924cf
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-04-09 14:10:48 +0900

    show image

---
+++ yolox-with-zedsdk/detector.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
+
+import argparse
+import torch
+import cv2
+import pyzed.sl as sl
+
+from time import sleep
+
+
+
+def main():
+
+    print("Initializing Camera...")
+
+    zed = sl.Camera()
+
+    input_type = sl.InputType()
+    if opt.svo is not None:
+        input_type.set_from_svo_file(opt.svo)
+
+    # Create a InitParameters object and set configuration parameters
+    init_params = sl.InitParameters(input_t=input_type, svo_real_time_mode=True)
+    init_params.coordinate_units = sl.UNIT.METER
+    init_params.depth_mode = sl.DEPTH_MODE.ULTRA  # QUALITY
+    init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
+    init_params.depth_maximum_distance = 50
+
+    runtime_params = sl.RuntimeParameters()
+    status = zed.open(init_params)
+
+    if status != sl.ERROR_CODE.SUCCESS:
+        print(repr(status))
+        exit()
+
+    image_left_tmp = sl.Mat()
+
+    print("Initialized Camera")
+
+
+    objects = sl.Objects()
+    obj_runtime_param = sl.ObjectDetectionRuntimeParameters()
+
+    # Display
+    camera_infos = zed.get_camera_information()
+    camera_res = camera_infos.camera_configuration.resolution
+    # Create OpenGL viewer
+    # viewer = gl.GLViewer()
+    point_cloud_res = sl.Resolution(min(camera_res.width, 720), min(camera_res.height, 404))
+    point_cloud_render = sl.Mat()
+    # viewer.init(camera_infos.camera_model, point_cloud_res, obj_param.enable_tracking)
+    point_cloud = sl.Mat(point_cloud_res.width, point_cloud_res.height, sl.MAT_TYPE.F32_C4, sl.MEM.CPU)
+    image_left = sl.Mat()
+
+    while 1:
+        # or viewer.is_available() and not exit_signal:
+        if zed.grab(runtime_params) == sl.ERROR_CODE.SUCCESS:
+            # -- Get the image
+            zed.retrieve_image(image_left, sl.VIEW.LEFT)
+            image_net = image_left.get_data()
+
+            cv2.imshow("ZED | 2D View and Birds View", image_net)
+            key = cv2.waitKey(10)
+            if key == 27:
+                exit_signal = True
+        else:
+            exit_signal = True
+
+    zed.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str, default='yolov8m.pt', help='model.pt path(s)')
+    parser.add_argument('--svo', type=str, default=None, help='optional svo file')
+    parser.add_argument('--img_size', type=int, default=416, help='inference size (pixels)')
+    parser.add_argument('--conf_thres', type=float, default=0.4, help='object confidence threshold')
+    opt = parser.parse_args()
+
+    with torch.no_grad():
+        main()
