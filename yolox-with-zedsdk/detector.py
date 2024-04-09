8784184c42282c5770e2216054b7da678550aeb0
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-04-09 15:11:35 +0900

    simlilar to demo.py

--- yolox-with-zedsdk/detector.py
+++ yolox-with-zedsdk/detector.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
 
 import argparse
+from loguru import logger
+import os
+
 import cv2
 import pyzed.sl as sl
 
+from yolox.data.data_augment import ValTransform
+from yolox.data.datasets import COCO_CLASSES
+from yolox.exp import get_exp
+from yolox.utils import fuse_model, get_model_info, postprocess, vis
 
-def main():
+from demo import Predictor
+from demo import make_parser,  get_exp
+
+def main(exp, args):
 
     print("Initializing Camera...")
 
     zed = sl.Camera()
 
     input_type = sl.InputType()
-    if opt.svo is not None:
-        input_type.set_from_svo_file(opt.svo)
 
     # Create a InitParameters object and set configuration parameters
-    init_params = sl.InitParameters(input_t=input_type, svo_real_time_mode=True)
+    init_params = sl.InitParameters()
     init_params.coordinate_units = sl.UNIT.METER
     init_params.depth_mode = sl.DEPTH_MODE.ULTRA  # QUALITY
     init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
@@ -36,14 +44,49 @@ def main():
     # Display
     image_left = sl.Mat()
 
+    if args.trt:
+        args.device = "gpu"
+
+    logger.info("Args: {}".format(args))
+
+    if args.conf is not None:
+        exp.test_conf = args.conf
+    if args.nms is not None:
+        exp.nmsthre = args.nms
+    if args.tsize is not None:
+        exp.test_size = (args.tsize, args.tsize)
+
+    model = exp.get_model()
+    logger.info("Model Summary: {}".format(get_model_info(model, exp.test_size)))
+
+    if args.device == "gpu":
+        model.cuda()
+        if args.fp16:
+            model.half()  # to FP16
+    model.eval()
+
+    if args.fuse:
+        logger.info("\tFusing model...")
+        model = fuse_model(model)
+
+    if 1:
+        trt_file = None
+        decoder = None
+
+    predictor = Predictor(
+        model, exp, COCO_CLASSES, trt_file, decoder,
+        "gpu", args.fp16, args.legacy,
+    )
     while 1:
         # or viewer.is_available() and not exit_signal:
         if zed.grab(runtime_params) == sl.ERROR_CODE.SUCCESS:
             # -- Get the image
             zed.retrieve_image(image_left, sl.VIEW.LEFT)
-            image_net = image_left.get_data()
+            frame = image_left.get_data()
+            outputs, img_info = predictor.inference(frame)
+            result_frame = predictor.visual(outputs[0], img_info, predictor.confthre)
 
-            cv2.imshow("ZED | 2D View and Birds View", image_net)
+            cv2.imshow("ZED | 2D View and Birds View", result_frame)
             key = cv2.waitKey(10)
             if key == 27:
                 exit_signal = True
@@ -54,11 +97,6 @@ def main():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='yolov8m.pt', help='model.pt path(s)')
-    parser.add_argument('--svo', type=str, default=None, help='optional svo file')
-    parser.add_argument('--img_size', type=int, default=416, help='inference size (pixels)')
-    parser.add_argument('--conf_thres', type=float, default=0.4, help='object confidence threshold')
-    opt = parser.parse_args()
-
-    main()
+    args = make_parser().parse_args()
+    exp = get_exp(args.exp_file, args.name)
+    main(exp, args)
