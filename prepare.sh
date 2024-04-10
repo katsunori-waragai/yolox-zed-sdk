3954b13e91a863ebec17966c20de0350d17b2aba
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-04-10 11:03:08 +0900

    yolox_s model only

--- prepare.sh
+++ prepare.sh
@@ -2,14 +2,10 @@
 # torch2trt
 cd /root
 git clone https://github.com/NVIDIA-AI-IOT/torch2trt ; cd torch2trt; python3 setup.py install
-cd /root/yolox-zed-sdk
-export TRTMODEL_S=YOLOX_outputs/yolox_s/model_trt.pth
+
+export PROJECT_HOME=/root/yolox-zed-sdk
+cd ${PROJECT_HOME}
+export TRTMODEL_S=${PROJECT_HOME}/YOLOX_outputs/yolox_s/model_trt.pth
 if [ ! -f ${TRTMODEL_S} ]; then
 	python3 tools/trt.py -n yolox-s -c yolox_s.pth
 fi
-
-export TRTMODEL_TINY=YOLOX_outputs/yolox_tiny/model_trt.pth
-if [ ! -f ${TRTMODEL_TINY} ]; then
-	python3 tools/trt.py -n yolox-tiny -c yolox_tiny.pth
-fi
-
