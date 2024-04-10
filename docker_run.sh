7166b76500b22c8d4d1bc955a193ec6ecaae7f9e
katsunori-waragai <huyuhiko1128@gmail.com>
2024-04-10 14:08:29 +0900

    added GIT_ROOT

--- docker_run.sh
+++ docker_run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+export GIT_ROOT=${HOME}/github
 sudo docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
-	-v ~/github/yolox-docker/YOLOX_outputs:/root/YOLOX/YOLOX_outputs \
-        -v ~/github/yolox-docker:/root/YOLOX/yolox-docker \
-	-v ~/github/yolox-zed-sdk:/root/yolox-zed-sdk \
+	-v ${GIT_ROOT}/yolox-docker/YOLOX_outputs:/root/YOLOX/YOLOX_outputs \
+  -v ${GIT_ROOT}/yolox-docker:/root/YOLOX/yolox-docker \
+	-v ${GIT_ROOT}/yolox-zed-sdk:/root/yolox-zed-sdk \
 	--device /dev/bus/usb \
 	--device /dev/video0:/dev/video0:mwr \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix yolox:100
