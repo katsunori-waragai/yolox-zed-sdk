4740627b18ef361696220190267401ef14e3b1da
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-04-10 10:12:12 +0900

    changed mount point for yolox-zed-sdk

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # torch2trt
-cd /root/YOLOX/yolox-zed-sdk
+cd /root
 git clone https://github.com/NVIDIA-AI-IOT/torch2trt ; cd torch2trt; python3 setup.py install
-cd /root/YOLOX/yolox-zed-sdk
+cd /root/yolox-zed-sdk
 export TRTMODEL_S=YOLOX_outputs/yolox_s/model_trt.pth
 if [ ! -f ${TRTMODEL_S} ]; then
 	python3 tools/trt.py -n yolox-s -c yolox_s.pth
