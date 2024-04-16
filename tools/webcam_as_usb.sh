76815396338251f65190b2e639f70a751d706f17
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-04-16 11:03:13 +0900

    use --trt

--- tools/webcam_as_usb.sh
+++ tools/webcam_as_usb.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd $(dirname $0)
-python3 demo_zed.py webcam -n yolox-s -c yolox_s.pth --conf 0.25 --nms 0.45 --device=gpu --as_USB
+# python3 demo_zed.py webcam -n yolox-s -c yolox_s.pth --conf 0.25 --nms 0.45 --device=gpu --as_USB
+python3 demo_zed.py webcam -n yolox-s --trt --conf 0.25 --nms 0.45 --device=gpu --as_USB
--- tools/webcam_zed.sh
+++ tools/webcam_zed.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd $(dirname $0)
 export MESA_GL_VERSION_OVERRIDE=3.3
-python3 demo_zed.py webcam -n yolox-s -c yolox_s.pth --conf 0.25 --nms 0.45 --device=gpu
+python3 demo_zed.py webcam -n yolox-s --trt --conf 0.25 --nms 0.45 --device=gpu
+# python3 demo_zed.py webcam -n yolox-s -c yolox_s.pth --conf 0.25 --nms 0.45 --device=gpu
