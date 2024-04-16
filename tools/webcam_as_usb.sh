c37b836ed4f924f4fdd10816e98d252abda29ca4
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-04-16 13:32:34 +0900

    get ready to trt

--- tools/webcam_as_usb.sh
+++ tools/webcam_as_usb.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd $(dirname $0)
+(pip3 list | grep torch2trt)  || ( cd .. ; make prepare)
 # python3 demo_zed.py webcam -n yolox-s -c yolox_s.pth --conf 0.25 --nms 0.45 --device=gpu --as_USB
 python3 demo_zed.py webcam -n yolox-s --trt --conf 0.25 --nms 0.45 --device=gpu --as_USB
