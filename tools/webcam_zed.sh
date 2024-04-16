30f496c08e429efc48187602072f845f8d4e1004
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-04-16 13:27:58 +0900

    be sure to make prepare

--- tools/webcam_zed.sh
+++ tools/webcam_zed.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd $(dirname $0)
 export MESA_GL_VERSION_OVERRIDE=3.3
+(pip3 list | grep torch2trt) || ( cd .. ; make prepare)
 python3 demo_zed.py webcam -n yolox-s --trt --conf 0.25 --nms 0.45 --device=gpu
 # python3 demo_zed.py webcam -n yolox-s -c yolox_s.pth --conf 0.25 --nms 0.45 --device=gpu
