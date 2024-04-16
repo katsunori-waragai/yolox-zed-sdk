8cbf19a967afab51370803c059d0b7acf41754f3
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-04-16 11:12:25 +0900

    use --trt (#20)
    why
    tensortRT 化したモデルをまだ使っていなかった。
    what
    tensortRT化したファイルを使うように設定した。
    tensortRT化したファイルをdemo_zed.py ファイル内で見つけるようにした。
    view_gl = False に設定した。

--- tools/webcam_as_usb.sh
+++ tools/webcam_as_usb.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-python3 demo_zed.py webcam -n yolox-s -c yolox_s.pth --conf 0.25 --nms 0.45 --device=gpu --as_USB
+cd $(dirname $0)
+# python3 demo_zed.py webcam -n yolox-s -c yolox_s.pth --conf 0.25 --nms 0.45 --device=gpu --as_USB
+python3 demo_zed.py webcam -n yolox-s --trt --conf 0.25 --nms 0.45 --device=gpu --as_USB
