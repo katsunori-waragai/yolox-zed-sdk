df9c1592bc9edad28186c4e831fc8590c2e8035d
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-11-20 15:56:11 +0900

    no wget in prepare.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 xhost +
-export GIT_ROOT=$(cd $(dirname $0)/.. ; pwd)
+export CWD=$(cd $(dirname $0)/; pwd)
 docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
-	-v ${GIT_ROOT}/yolox-zed-sdk:/root/yolox-zed-sdk \
+	-v ${CWD}:/root/yolox-zed-sdk \
 	--device /dev/bus/usb \
 	--device /dev/video0:/dev/video0:mwr \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix yolox-zed:100
--- prepare.sh
+++ prepare.sh
@@ -2,12 +2,12 @@
 export PROJECT_HOME=/root/yolox-zed-sdk
 export YOLOX_HOME=/root/YOLOX
 cd ${YOLOX_HOME}
-if [ ! -f yolox_s.pth ]; then
-  wget https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_s.pth
-fi
+#if [ ! -f yolox_s.pth ]; then
+#  wget https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_s.pth
+#fi
 export TRTMODEL_S=${YOLOX_HOME}/YOLOX_outputs/yolox_s/model_trt.pth
 if [ ! -f ${TRTMODEL_S} ]; then
-	python3 tools/trt.py -n yolox-s -c yolox_s.pth
+	python3 tools/trt.py -n yolox-s -c ${YOLOX_HOME}/yolox_s.pth
 else
   echo "skipped conversion by torch2trt. already exists."
 fi
