f9a6aa7359ae3523d06dbe8e52a14f7ca1819662
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-11-20 16:04:32 +0900

    simplified

--- prepare.sh
+++ prepare.sh
@@ -2,16 +2,14 @@
 export PROJECT_HOME=/root/yolox-zed-sdk
 export YOLOX_HOME=/root/YOLOX
 cd ${YOLOX_HOME}
-#if [ ! -f yolox_s.pth ]; then
-#  wget https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_s.pth
-#fi
-export TRTMODEL_S=${YOLOX_HOME}/YOLOX_outputs/yolox_s/model_trt.pth
+mkdir --parents ${PROJECT_HOME}/YOLOX_outputs/yolox_s
+export TRTMODEL_S=${PROJECT_HOME}/YOLOX_outputs/yolox_s/yolox_s/model_trt.pth
+ls ${TRTMODEL_S}
 if [ ! -f ${TRTMODEL_S} ]; then
 	python3 tools/trt.py -n yolox-s -c ${YOLOX_HOME}/yolox_s.pth
 else
   echo "skipped conversion by torch2trt. already exists."
 fi
 
-mkdir --parents ${PROJECT_HOME}//YOLOX_outputs/yolox_s
-cp -r ${YOLOX_HOME}/YOLOX_outputs/yolox_s/ ${PROJECT_HOME}/YOLOX_outputs/yolox_s
+#cp -r ${YOLOX_HOME}/YOLOX_outputs/yolox_s/ ${PROJECT_HOME}/YOLOX_outputs/yolox_s
 
