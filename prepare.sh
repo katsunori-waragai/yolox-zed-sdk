888642ae768aa91c915dc5b47ad58053badcd193
katsunori-waragai <huyuhiko1128@gmail.com>
2024-11-20 10:52:37 +0900

    changed echo message

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,8 @@
 cd /root
 if [ ! -d torch2trt ]; then
   git clone https://github.com/NVIDIA-AI-IOT/torch2trt ; cd torch2trt; python3 setup.py install
+else
+  echo "skipped install torch2trt. already exists."
 fi
 
 export PROJECT_HOME=/root/yolox-zed-sdk
@@ -14,7 +16,7 @@ export TRTMODEL_S=${YOLOX_HOME}/YOLOX_outputs/yolox_s/model_trt.pth
 if [ ! -f ${TRTMODEL_S} ]; then
 	python3 tools/trt.py -n yolox-s -c yolox_s.pth
 else
-  echo already exist ${TRTMODEL_S}
+  echo "skipped conversion by torch2trt. already exists."
 fi
 
 mkdir --parents ${PROJECT_HOME}//YOLOX_outputs/yolox_s
