eb8cf2773fb625ca1784c5a637b8b09db7b9ba0f
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-11-21 12:27:20 +0900

    changed mount condition

--- tools/docker_run.sh
+++ tools/docker_run.sh
@@ -2,7 +2,7 @@
 xhost +
 export CWD=$(cd $(dirname $0)/; pwd)
 docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
-	-v ${CWD}:/root/yolox-zed-sdk \
+	-v ${CWD}:/root/yolox-zed-sdk/tools \
 	--device /dev/bus/usb \
 	--device /dev/video0:/dev/video0:mwr \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix yolox-zed:100
