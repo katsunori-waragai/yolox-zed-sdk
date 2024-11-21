5ddeb30998058f19d7e00dbad243fb8df4d60555
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-11-21 12:08:05 +0900

    still under develop

--- tools/prepare.sh
+++ tools/prepare.sh
@@ -6,7 +6,8 @@ if [ ! -f yolox_s.pth ]; then
 fi
 export TRTMODEL_S=${BINDING_HOME}/YOLOX_outputs/yolox_s/model_trt.pth
 if [ ! -f ${TRTMODEL_S} ]; then
-	python3 trt.py -n yolox-s -c yolox_s.pth
+  cd .. ;
+	python3 tools/trt.py -n yolox-s -c yolox_s.pth
 else
   echo "skipped conversion by torch2trt. already exists."
 fi
