80f7ed666b67d5f0abbf82acab6a26f00e9e84d2
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-11-21 13:44:39 +0900

    Feature/pytorch yolox (#52)
    why
    I want a folder structure that can be part of the ZED SDK.
    what
    Renamed the folder.
    Checking the operation
    I ran prepare.sh and checked the behavior of webcam_zed.sh.

--- pytorch_yolox/prepare.sh
+++ pytorch_yolox/prepare.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
-export BINDING_HOME=/root/yolox-zed-sdk/tools
+export BINDING_HOME=/root/yolox-zed-sdk/pytorch_yolox
 export YOLOX_HOME=/root/YOLOX
+mkdir --parents ${BINDING_HOME}/YOLOX_outputs/yolox_s
+mkdir --parents ${YOLOX_HOME}/YOLOX_outputs/yolox_s
+
 cd ${YOLOX_HOME}
 if [ ! -f yolox_s.pth ]; then
   wget https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_s.pth
 fi
 export TRTMODEL_S=${YOLOX_HOME}/YOLOX_outputs/yolox_s/model_trt.pth
 if [ ! -f ${TRTMODEL_S} ]; then
-	python3 tools/trt.py -n yolox-s -c yolox_s.pth
+  cd ${YOLOX_HOME}/
+  python3 tools/trt.py -n yolox-s -c ${YOLOX_HOME}/yolox_s.pth
 else
   echo "skipped conversion by torch2trt. already exists."
 fi
 
-mkdir --parents ${BINDING_HOME}//YOLOX_outputs/yolox_s
 cp -r ${YOLOX_HOME}/YOLOX_outputs/yolox_s/ ${BINDING_HOME}/YOLOX_outputs/yolox_s
 
