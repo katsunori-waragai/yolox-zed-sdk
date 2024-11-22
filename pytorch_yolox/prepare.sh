696948b5d20deb84ee24970ab6e7279880b93c82
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-11-22 15:08:00 +0900

    removed local trt.py

--- pytorch_yolox/prepare.sh
+++ pytorch_yolox/prepare.sh
@@ -10,8 +10,7 @@ if [ ! -f yolox_s.pth ]; then
 fi
 export TRTMODEL_S=${YOLOX_HOME}/YOLOX_outputs/yolox_s/model_trt.pth
 if [ ! -f ${TRTMODEL_S} ]; then
-  cd ${YOLOX_HOME}/
-  python3 tools/trt.py -n yolox-s -c ${YOLOX_HOME}/yolox_s.pth
+  cd ${YOLOX_HOME}/; python3 tools/trt.py -n yolox-s -c ${YOLOX_HOME}/yolox_s.pth
 else
   echo "skipped conversion by torch2trt. already exists."
 fi
